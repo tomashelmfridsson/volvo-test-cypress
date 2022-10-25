f4d129329eac55d89ae3a8046e8c830b8b88d13d
tomas.helmfridsson <tomas.helmfridssson@bambuser.com>
2022-10-25 23:17:02 +0200

    Manage test cases to separate spec files

--- cypress/e2e/volvo/campaign_lighthouse.cy.js
+++ cypress/e2e/volvo/campaign_lighthouse.cy.js
@@ -1,19 +1,7 @@
 /// <reference types="@cypress-audit/lighthouse" />
 
-describe("campaign", () => {
-  it.only("Accept trust", () => {
-    cy.clearCookies();
-    cy.visit("https://www.volvocars.com/intl/v/car-safety/a-million-more/", {
-      // volvocars is blocked for robots, this is a workaround
-      headers: {
-        accept: "application/json, text/plain, */*",
-        "user-agent": "axios/0.27.2",
-      },
-    });
-    cy.get("#onetrust-accept-btn-handler").click();
-  });
-
-  it.skip('lighthouse audits', () => {
+describe("campaign Lighthouse", () => {
+  it('lighthouse audits', () => {
     cy.visit("https://www.volvocars.com/intl/v/car-safety/a-million-more/", {
       // volvocars is blocked for robots, this is a workaround
       headers: {
